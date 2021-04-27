c5cbad3cf55faeeca97864dcebde62dc4fada9da
Chauncey McAskill <chauncey@locomotive.ca>
2021-04-27 12:10:33 -0400

    Add support for Composer v2

--- src/Charcoal/Composer/InstallerPlugin.php
+++ src/Charcoal/Composer/InstallerPlugin.php
@@ -13,4 +13,14 @@ class InstallerPlugin implements PluginInterface
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // no need to deactivate anything
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // no need to uninstall anything
+    }
 }
